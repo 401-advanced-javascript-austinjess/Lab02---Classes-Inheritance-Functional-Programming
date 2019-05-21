804d96a7e94fb5d9a6c11797ecf85a0a094da8e0
Austin Jess <ajess33@gmail.com>
2019-05-21 15:51:57 -0500

    car and motorcycle class constructor

--- objects/index.js
+++ objects/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const VehicleConstructor = require('./vehicle-constructor.js');
+const VehicleClass = require('./vehicle-class');
 
 // Implement a car and motorcycle using a Constructor
 const mazda = new VehicleConstructor.Car('Mazda 3');
@@ -10,6 +11,10 @@ const harley = new VehicleConstructor.Motorcycle('Harley');
 console.log(harley.name, harley.wheelie(), harley.stop());
 
 // Implement a car and motorcycle using a Class
+const altima = new VehicleClass.CarClass('Altima');
+console.log(altima.name, altima.drive(), altima.stop());
 
+const bmw = new VehicleClass.MotorcycleClass('BMW');
+console.log(bmw.name, bmw.wheelie(), bmw.stop());
 
 // Implement a car and motorcycle using a Factory
